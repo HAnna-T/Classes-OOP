09f3e4b43bc56853b09d2961b3732429bf0e1dbe
Anna Harutyunyan <anuanharutyunyan>
2022-01-09 21:14:23 +0400

    Add: Inheritance between classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,8 +11,32 @@ class Product {
     this.price = price;
   }
 }
+class ElementAttribute {
+  constructor(attrName, attrValue) {
+    this.name = attrName;
+    this.value = attrValue;
+  }
+}
+class Component {
+  constructor(renderHookId) {
+    this.hookId = renderHookId;
+  }
 
-class ShoppingCart {
+  createRootElement(tag, cssClasses, attributes) {
+    const rootElement = document.createElement(tag);
+    if (cssClasses) {
+      rootElement.className = cssClasses;
+    }
+    if (attributes && attributes.length > 0) {
+      for (const attr of attributes) {
+        rootElement.setAttribute(attr.name, attr.value);
+      }
+    }
+    document.getElementById(this.hookId).append(rootElement);
+    return rootElement;
+  }
+}
+class ShoppingCart extends Component {
   items = [];
 
   set cartItems(value) {
@@ -29,6 +53,9 @@ class ShoppingCart {
     );
     return sum;
   }
+  constructor(renderHookId){
+    super(renderHookId)
+  }
 
   addProduct(product) {
     const updatedItems = [...this.items];
@@ -37,14 +64,12 @@ class ShoppingCart {
   }
 
   render() {
-    const cartEl = document.createElement("section");
+   const cartEl= this.createRootElement('section','cart');
     cartEl.innerHTML = `
       <h2>Total: \$${0}</h2>
       <button>Order Now!</button>
     `;
-    cartEl.className = "cart";
     this.totalOutput = cartEl.querySelector("h2");
-    return cartEl;
   }
 }
 
@@ -111,12 +136,12 @@ class Shop {
   render() {
     const renderHook = document.getElementById("app");
 
-    this.cart = new ShoppingCart();
-    const cartEl = this.cart.render();
+    this.cart = new ShoppingCart('app');
+    this.cart.render();
     const productList = new ProductList();
     const prodListEl = productList.render();
 
-    renderHook.append(cartEl);
+  
     renderHook.append(prodListEl);
   }
 }
