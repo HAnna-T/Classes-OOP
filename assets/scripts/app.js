87b2b02ae724fdcf94b67ebe97cb27843927e301
Anna Harutyunyan <anuanharutyunyan>
2022-01-09 21:29:13 +0400

    Add: Super method in constructor

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -73,8 +73,9 @@ class ShoppingCart extends Component {
   }
 }
 
-class ProductItem {
-  constructor(product) {
+class ProductItem extends Component{
+  constructor(product,renderHookId) {
+    super(renderHookId)
     this.product = product;
   }
 
@@ -83,8 +84,7 @@ class ProductItem {
   }
 
   render() {
-    const prodEl = document.createElement("li");
-    prodEl.className = "product-item";
+    const prodEl = this.createRootElement('li','product-item')
     prodEl.innerHTML = `
         <div>
           <img src="${this.product.imageUrl}" alt="${this.product.title}" >
@@ -98,11 +98,10 @@ class ProductItem {
       `;
     const addCartButton = prodEl.querySelector("button");
     addCartButton.addEventListener("click", this.addToCart.bind(this));
-    return prodEl;
   }
 }
 
-class ProductList {
+class ProductList extends Component {
   products = [
     new Product(
       "A Pillow",
@@ -118,31 +117,30 @@ class ProductList {
     ),
   ];
 
-  constructor() {}
+  constructor(renderHookId) {
+    super(renderHookId)
+  }
 
   render() {
-    const prodList = document.createElement("ul");
-    prodList.className = "product-list";
+     this.createRootElement('ul','product-list',[new ElementAttribute('id','prod-list')])
     for (const prod of this.products) {
-      const productItem = new ProductItem(prod);
-      const prodEl = productItem.render();
-      prodList.append(prodEl);
+      const productItem = new ProductItem(prod,'prod-list');
+       productItem.render();
+    
     }
-    return prodList;
+    
   }
 }
 
 class Shop {
   render() {
-    const renderHook = document.getElementById("app");
 
     this.cart = new ShoppingCart('app');
     this.cart.render();
-    const productList = new ProductList();
-    const prodListEl = productList.render();
+    const productList = new ProductList('app');
+   productList.render();
 
   
-    renderHook.append(prodListEl);
   }
 }
 
